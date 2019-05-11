b8035d7b7449b23a5c38c0da54a7a484114fc07a
Xueying Guo <48356714+Joanne-Kwok@users.noreply.github.com>
2019-05-12 02:56:53 +0930

    Create CIDIDS.scala

---
+++ Implementation(BDCA)/Xueying Guo/Run on Openstack/CIDIDS/CIDIDS.scala
@@ -0,0 +1,194 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.linalg.{Vectors,Vector}
+import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import java.io.PrintWriter;
+
+object CIDIDS {
+  
+    def main(args: Array[String]): Unit = {
+    //val conf = new SparkConf().setAppName("SVMWithSGDExample").setMaster("local")
+    //val conf = new SparkConf().setAppName("TestSVM").setMaster("local")
+    //val conf = new SparkConf().setAppName("TestSVM").setMaster("local")
+    val conf = new SparkConf().setAppName("CIDIDS")
+    val sc = new SparkContext(conf)
+
+    println("------------------------Read File-------------------------")
+    val traindata = sc.textFile("/user/ubuntu/spark/CIDIDS/CIDIDSTrain.txt")   
+    val testdata = sc.textFile("/user/ubuntu/spark/CIDIDS/CIDIDSTest.txt")
+    //val traindata = sc.textFile("hdfs://localhost:9000/user/hadoop/spark/kdd/20KDDTrain.txt")   
+    //val testdata = sc.textFile("hdfs://localhost:9000/user/hadoop/spark/kdd/20KDDTest.txt")
+
+
+    println("-------------------Read file Complete -> Set LabelPoint----------------------")
+    val training = traindata.map { line =>
+      val parts = line.split(',')
+        LabeledPoint(
+            parts(11).toDouble,
+            Vectors.dense(
+                if ( parts(0) == "EXTSERVER") 99999.toDouble
+                else parts(0).toDouble,
+                if ( parts(1) == "EXTSERVER") 99999.toDouble
+                else parts(1).toDouble,
+                if ( parts(2) == "EXTSERVER") 99999.toDouble
+                else parts(2).toDouble,
+                if ( parts(3) == "EXTSERVER") 99999.toDouble
+                else parts(3).toDouble,
+                if ( parts(4) == "EXTSERVER") 99999.toDouble
+                else parts(4).toDouble,
+                if ( parts(5) == "EXTSERVER") 99999.toDouble
+                else parts(5).toDouble,
+                if ( parts(6) == "EXTSERVER") 99999.toDouble
+                else parts(6).toDouble,
+                if ( parts(7) == "EXTSERVER") 99999.toDouble
+                else parts(7).toDouble,
+                if ( parts(8) == "EXTSERVER") 99999.toDouble
+                else parts(8).toDouble,
+                if ( parts(9) == "EXTSERVER") 99999.toDouble
+                else parts(9).toDouble,
+                if ( parts(10) == "EXTSERVER") 99999.toDouble
+                else parts(10).toDouble
+                )
+	)
+       }
+
+    // Run training algorithm to build the model
+    println("--------------------SetLable Complete -> TrainModel----------------------")
+    val numIterations = 1000
+    
+    val trainstarttime = System.currentTimeMillis()
+    //new SimpleDateFormat("yyyy-MM-dd HH:ss:SSS").format(new Date())
+    
+    val model = SVMWithSGD.train(training, numIterations)
+    
+    val trainendtime = System.currentTimeMillis()
+    
+    val trainingtime = trainendtime - trainstarttime
+    
+    println("Training Time = " + trainingtime + " millisecond")
+
+    println("-------------------Trainning Complete -> Testing Label----------------------")
+    val testing = testdata.map { line =>
+      val parts = line.split(',')
+        LabeledPoint(
+            parts(11).toDouble,
+            Vectors.dense(
+                if ( parts(0) == "EXTSERVER") 99999.toDouble
+                else parts(0).toDouble,
+                if ( parts(1) == "EXTSERVER") 99999.toDouble
+                else parts(1).toDouble,
+                if ( parts(2) == "EXTSERVER") 99999.toDouble
+                else parts(2).toDouble,
+                if ( parts(3) == "EXTSERVER") 99999.toDouble
+                else parts(3).toDouble,
+                if ( parts(4) == "EXTSERVER") 99999.toDouble
+                else parts(4).toDouble,
+                if ( parts(5) == "EXTSERVER") 99999.toDouble
+                else parts(5).toDouble,
+                if ( parts(6) == "EXTSERVER") 99999.toDouble
+                else parts(6).toDouble,
+                if ( parts(7) == "EXTSERVER") 99999.toDouble
+                else parts(7).toDouble,
+                if ( parts(8) == "EXTSERVER") 99999.toDouble
+                else parts(8).toDouble,
+                if ( parts(9) == "EXTSERVER") 99999.toDouble
+                else parts(9).toDouble,
+                if ( parts(10) == "EXTSERVER") 99999.toDouble
+                else parts(10).toDouble)
+        )
+       }
+
+    // Clear the default threshold.
+    model.clearThreshold()
+    model.setThreshold(0.0)
+    
+    println("--------------Test Label Complete -> Predict----------------------")
+    
+    val teststarttime = System.currentTimeMillis()
+    
+    // Compute raw scores on the test set.
+    val scoreAndLabels = testing.map { point =>
+      val score = model.predict(point.features)
+      (score, point.label)
+    }
+    
+    val testendtime = System.currentTimeMillis()
+    
+    val testtime = testendtime - teststarttime
+    
+    println("prediction Time = " + testtime + " millisecond")
+    
+    //normal is 0 and abnormal is 1, so true positive(tp) is when predict = 1 and correct = 1
+    val tp = testing.filter { e =>
+      val predict = model.predict(e.features)
+      val correct = e.label
+      predict == 1.toDouble && correct == 1.toDouble
+    }.count
+
+    //normal is 0 and abnormal is 1, so false positive(fp) is when predict = 1 and correct = 0
+    val fp = testing.filter { e =>
+      val predict = model.predict(e.features)
+      val correct = e.label
+      predict == 1.toDouble && correct == 0.toDouble
+    }.count
+    
+    //normal is 0 and abnormal is 1, so true negative(tn) is when predict = 0 and correct = 0
+    val tn = testing.filter { e =>
+      val predict = model.predict(e.features)
+      val correct = e.label
+      predict == 0.toDouble && correct == 0.toDouble
+    }.count
+    
+    //normal is 0 and abnormal is 1, so false negative(fn) is when predict = 0 and correct = 1
+    val fn = testing.filter { e =>
+      val predict = model.predict(e.features)
+      val correct = e.label
+      predict == 0.toDouble && correct == 1.toDouble
+    }.count
+    
+    val accuratecount = tn + tp
+    val accuracy=accuratecount.toDouble/(tn+tp+fn+fp).toDouble
+    val precision = tp.toDouble/(tp+fp).toDouble
+    val POS = tp + fn
+    val NEG = tn + fp
+    val FPR = fp.toDouble/NEG.toDouble //false positive rate
+    val TPR = tp.toDouble/POS.toDouble //true positive rate
+    val TNR = tn.toDouble/NEG.toDouble //true negative rate
+    val FNR = fn.toDouble/POS.toDouble //false negative rate
+    val R = tp.toDouble/(tp+fn).toDouble // recall rate
+    val f1 = 2.toDouble/(1.toDouble/precision.toDouble + 1.toDouble/R.toDouble).toDouble
+    val dr = tp.toDouble / (tp+fp).toDouble
+    //scoreAndLabels.foreach(println)
+    
+    println("accurate count = " + accuratecount )
+    println("total count =" +testdata.count )
+    println("true positive (tp)= " + tp )
+    println("false positive (fp)= " + fp )
+    println("true negative (tn)= " + tn )
+    println("false negative (fn)= " + fn )
+    println("accuracy = " + accuracy.toDouble )
+    println("precision = " + precision.toDouble)
+    println("POS = " + POS)
+    println("NEG = " + NEG)
+    println("FPR = " + FPR.toDouble)
+    println("TPR = " + TPR.toDouble)
+    println("TNR = " + TNR.toDouble)
+    println("FNR = " + FNR.toDouble)
+    println("Detection Rate = " + dr.toDouble)
+    println("F1 Score = " + f1.toDouble)
+    
+    println("----------------------------Complete----------------------")
+
+    // Get evaluation metrics.
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+    //val metrics = new MulticlassMetrics(scoreAndLabels)
+    val auROC = metrics.areaUnderROC()
+
+    println("Area under ROC = " + auROC)
+    
+    }
+}
