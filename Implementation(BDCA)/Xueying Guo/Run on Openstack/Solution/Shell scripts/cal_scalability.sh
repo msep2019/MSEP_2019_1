fc31e26c36ca3ec6341673f17d1eaad1da5bb99a
Xueying Guo <48356714+Joanne-Kwok@users.noreply.github.com>
2019-06-21 11:55:44 +0930

    update formula to quantify the scalability

--- Implementation(BDCA)/Xueying Guo/Run on Openstack/Solution/Shell scripts/cal_scalability.sh
+++ Implementation(BDCA)/Xueying Guo/Run on Openstack/Solution/Shell scripts/cal_scalability.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#Parameter setting
-
 #usage: ./cal_scalability.sh <number_of_pair> <node_number1> <runtime_1> ... <node_number_n> <runtime_n>
 #example: ./cal_scalability.sh 3 2 22345 4 13456 6 23456 8 23456 10 23456
 #Description: This program for calculate the scalability by using Least Squares Method
+
+#Parameter setting
 
 PARAM_PATH="/home/hadoop/Desktop/scripts/param"
 . $PARAM_PATH/env.param
@@ -39,8 +39,23 @@ y3=$7
 y4=$9
 y5=${11}
 
-sumx=$((${x1}+${x2}+${x3}+${x4}+${x5}))
-sumy=$((${y1}+${y2}+${y3}+${y4}+${y5}))
+#parameter setting
+#ideal decrease 50% runtime compare with the original time
+threshold=0.5
+decthreshold=1
+inc=0.1
+dec=0.1
+
+slpwht=0.75
+incnodewht=0.1
+ttldecwht=0.1
+stablescorewht=0.05
+
+
+#Calculate actual slope for fitting curve
+
+sumx=$(awk 'BEGIN{print '${x1}' + '${x2}' + '${x3}' + '${x4}' + '${x5}'}')
+sumy=$(awk 'BEGIN{print '${y1}' + '${y2}' + '${y3}' + '${y4}' + '${y5}'}')
 
 avgx=$(awk 'BEGIN{print '${sumx}'/'${PAIRS}' }')
 avgy=$(awk 'BEGIN{print '${sumy}'/'${PAIRS}' }')
@@ -66,9 +81,141 @@ echo "k=${k}"
 echo "b=${b}" 
 echo "formular is: y=${k}x+${b}"
 
-echo "k=${k}" > ${LOG_TMP_PATH}/scalability.txt
-echo "b=${b}" >> ${LOG_TMP_PATH}/scalability.txt
-echo "formular is:y=${k}x+${b}" >> ${LOG_TMP_PATH}/scalability.txt
+
+#Calculate ideal slope by using Least Squares Method
+sy1=${y1}
+sy2=$(awk 'BEGIN{print '${y1}'*'${threshold}' }')
+
+idealk=$(awk 'BEGIN{print ('${sy2}'-'${sy1}')/('${x5}'-'${x1}') }')
+#idealk=$(awk 'BEGIN{print ('${sy2}'-'${sy1}') }')
+
+echo "sy1=${sy1}" 
+echo "sy2=${sy2}" 
+echo "idealk=${idealk}" 
+
+#Calculate weight slope score
+
+# if [ $(echo "${k} > 0"|bc) -eq 1 ]; then
+#  slopescore=0
+# else
+  slopescore=$(awk 'BEGIN{print '${k}' / '${idealk}' }')
+# fi
+
+wslopescore=$(awk 'BEGIN{print '${slopescore}' * '${slpwht}' }')
+
+echo "slope score=${slopescore}" 
+echo "slope score weight=${wslopescore}" 
+
+#Calculate total decrease score
+
+idealdec=$(awk 'BEGIN{print '${y1}'*'${decthreshold}' }')
+actdec=$(awk 'BEGIN{print '${y1}'-'${y5}' }')
+
+decscore=$(awk 'BEGIN{print '${actdec}' / '${idealdec}' }')
+wdecscore=$(awk 'BEGIN{print '${decscore}' * '${ttldecwht}' }')
+
+echo "total decrease score=${decscore}" 
+echo "total decrease score weight=${wdecscore}" 
+
+#Calculate decrease each node score
+
+incnodescore=0
+
+gap=$(awk 'BEGIN{print '${y5}'-'${y4}' }')
+nodegap=$(awk 'BEGIN{print '${x5}'-'${x4}' }')
+maxgap=$(awk 'BEGIN{print '${y4}'*'${inc}' * '${nodegap}' }')
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  incnodescore=$(awk 'BEGIN{print '${incnodescore}' - 0.25 }')
+ fi
+
+gap=$(awk 'BEGIN{print '${y4}'-'${y3}' }')
+nodegap=$(awk 'BEGIN{print '${x4}'-'${x3}' }')
+maxgap=$(awk 'BEGIN{print '${y3}'*'${inc}' * '${nodegap}' }')
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  incnodescore=$(awk 'BEGIN{print '${incnodescore}' - 0.25 }')
+ fi
+
+gap=$(awk 'BEGIN{print '${y3}'-'${y2}' }')
+nodegap=$(awk 'BEGIN{print '${x3}'-'${x2}' }')
+maxgap=$(awk 'BEGIN{print '${y2}'*'${inc}' * '${nodegap}' }')
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  incnodescore=$(awk 'BEGIN{print '${incnodescore}' - 0.25 }')
+ fi
+
+gap=$(awk 'BEGIN{print '${y2}'-'${y1}' }')
+nodegap=$(awk 'BEGIN{print '${x2}'-'${x1}' }')
+maxgap=$(awk 'BEGIN{print '${y1}'*'${inc}' * '${nodegap}' }')
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  incnodescore=$(awk 'BEGIN{print '${incnodescore}' - 0.25 }')
+ fi
+
+wincnodescore=$(awk 'BEGIN{print '${incnodescore}' * '${incnodewht}' }')
+
+echo "increase score=${incnodescore}" 
+echo "increase score weight=${wincnodescore}" 
+
+#Calculate stable decrease
+
+stablescore=0
+
+gap=$(awk 'BEGIN{print '${y4}'-'${y5}' }')
+nodegap=$(awk 'BEGIN{print '${x5}'-'${x4}' }')
+maxgap=$(awk 'BEGIN{print '${y4}'*'${dec}' * '${nodegap}' }' )
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+ fi
+
+gap=$(awk 'BEGIN{print '${y3}'-'${y4}' }')
+nodegap=$(awk 'BEGIN{print '${x4}'-'${x3}' }')
+maxgap=$(awk 'BEGIN{print '${y3}'*'${dec}' * '${nodegap}' }')
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+ fi
+
+gap=$(awk 'BEGIN{print '${y2}'-'${y3}' }')
+nodegap=$(awk 'BEGIN{print '${x3}'-'${x2}' }')
+maxgap=$(awk 'BEGIN{print '${y2}'*'${dec}' * '${nodegap}' }')
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+ fi
+
+gap=$(awk 'BEGIN{print '${y1}'-'${y2}' }')
+nodegap=$(awk 'BEGIN{print '${x2}'-'${x1}' }')
+maxgap=$(awk 'BEGIN{print '${y1}'*'${dec}'*'${nodegap}' }')
+ if [ $(echo "${maxgap} <= ${gap}"|bc) -eq 1 ]; then
+  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+ fi
+
+# if [ $(echo "${y5} < ${y4}"|bc) -eq 1 ]; then
+#  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+# fi
+
+# if [ $(echo "${y4} < ${y3}"|bc) -eq 1 ]; then
+#  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+# fi
+
+# if [ $(echo "${y3} < ${y2}"|bc) -eq 1 ]; then
+#  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+# fi
+
+# if [ $(echo "${y2} < ${y1}"|bc) -eq 1 ]; then
+#  stablescore=$(awk 'BEGIN{print '${stablescore}' + 0.25 }')
+# fi
+
+wstablescore=$(awk 'BEGIN{print '${stablescore}' * '${stablescorewht}' }')
+
+echo "stable score=${stablescore}" 
+echo "stable score weight=${wstablescore}" 
+
+
+#Calculate final score for scalability
+scalability=$(awk 'BEGIN{print '${wslopescore}' + '${wdecscore}' + '${wincnodescore}' + '${wstablescore}' }')
+echo "scalability=${scalability}" 
+
+#echo "k=${k}" > ${LOG_TMP_PATH}/scalability.txt
+#echo "b=${b}" >> ${LOG_TMP_PATH}/scalability.txt
+#echo "formular is:y=${k}x+${b}" >> ${LOG_TMP_PATH}/scalability.txt
+echo "scalability=${scalability}" > ${LOG_TMP_PATH}/scalability.txt
 
 exit 0
 
